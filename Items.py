97abd087313816a6512d47af438972ed58ec9248
Natalie <69278953+infires719@users.noreply.github.com>
2022-04-22 12:37:12 -0500

    Update Items.py
    Updated variable names to match database

--- Items.py
+++ Items.py
@@ -24,14 +24,15 @@ cursor.execute('SELECT * FROM dbo.UserLogins')
 def addItem():
     msg = ''
     #Gets item information from form
-    if requests.method == 'POST' and 'itemId' in requests.form and 'itemName' in requests.form and 'itemPrice' in requests.form and 'itemQuantity' in requests.form:
+    if requests.method == 'POST' and 'itemId' in requests.form and 'itemName' in requests.form and 'itemPrice' in requests.form and 'quantity' in requests.form and 'itemImage' in requests.form:
         itemId = requests['itemId']
         itemName = requests['itemName']
         itemPrice = requests['itemPrice']
-        itemQuantity = requests['itemQuantity']
+        quantity = requests['quantity']
+        itemImage = requests['itemImage']
 
         #Get row where itemId is the same from the form
-        cursor.execute('SELECT * FROM Items WHERE itemid = ?', itemId)
+        cursor.execute('SELECT * FROM Items WHERE itemId = ?', itemId)
         item = cursor.fetchone()
 
         #if item is not none, then the item already exists!
@@ -39,7 +40,7 @@ def addItem():
             msg = 'This item already exists! If you want to change an item, please update instead!'
         #itemId doesnt exist, add new entry to Items
         else:
-            cursor.execute("INSERT INTO dbo.Items(itemid, itemname, itemprice, quantity) VALUES(?, ?, ?, ?)", itemId, itemName, itemPrice, itemQuantity)
+            cursor.execute("INSERT INTO dbo.Items(itemId, itemName, itemPrice, quantity, itemImage) VALUES(?, ?, ?, ?, ?)", itemId, itemName, itemPrice, quantity, itemImage)
             conn.commit()
 
     #add print statement later!
@@ -55,12 +56,12 @@ def removeItem():
         itemId = requests['itemId']
 
         #Get row where itemId is the same as form itemId
-        cursor.execute('SELECT * FROM Items WHERE itemid = ?', itemId)
+        cursor.execute('SELECT * FROM Items WHERE itemId = ?', itemId)
         item = cursor.fetchone()
 
         #if item is not none, then the item already exists!
         if item:        
-            cursor.execute('DELETE FROM Items WHERE itemid = ?', itemId)
+            cursor.execute('DELETE FROM Items WHERE itemId = ?', itemId)
             conn.commit()
         #itemId doesnt exist, print error message
         else:
@@ -80,17 +81,17 @@ def updateItem():
         itemId = requests['itemId']
         itemName = requests['itemName']
         itemPrice = requests['itemPrice']
-        itemQuantity = requests['itemQuantity']
+        quantity = requests['quantity']
 
         #Get row where itemId is the same as form itemId
-        cursor.execute('SELECT * FROM Items WHERE itemid = ?', itemId)
+        cursor.execute('SELECT * FROM Items WHERE itemId = ?', itemId)
         item = cursor.fetchone()
 
         #if item is not none, then the item already exists!
         if item:
-            cursor.execute('SELECT quantity FROM Items WHERE itemid = ?', itemId)
+            cursor.execute('SELECT quantity FROM Items WHERE itemId = ?', itemId)
             oldQuantity = cursor.fetchone()
-            cursor.execute('UPDATE Items SET itemname = ?, itemprice = ?, quantity = ? + ? WHERE itemid = ?', itemName, itemPrice, itemQuantity, oldQuantity, itemId)
+            cursor.execute('UPDATE Items SET itemName = ?, itemPrice = ?, quantity = ? + ? WHERE itemId = ?', itemName, itemPrice, quantity, oldQuantity, itemId)
             conn.commit()
         #itemId doesnt exist, do not update anything and send an error message!
         else:
@@ -112,15 +113,15 @@ def searchItems(choice):
         if choice == "description":
             #Add percent to front and end of string so it searches in any position
             likeDescrip = '%' + itemDesc + '%'
-            cursor.execute('SELECT * FROM Items WHERE itemname LIKE ?', likeDescrip)
+            cursor.execute('SELECT * FROM Items WHERE itemName LIKE ?', likeDescrip)
         else:
             #Add percent to front and end of string so it searches in any position
             likeDescrip = '%' + itemName + '%'
-            cursor.execute('SELECT * FROM Items WHERE itemname LIKE ?', itemName)
+            cursor.execute('SELECT * FROM Items WHERE itemName LIKE ?', itemName)
 
         items = cursor.fetchall()
 
-        # if there are items -> return to new database? Or return True but populate a new database and return false so frontend ppl can add html message or what not?
+        # if there are items -> return to new database
         if items:
             return render_template(''), items
         else
@@ -132,7 +133,7 @@ def searchItems(choice):
 # Order should be DESC or ASC ->Pass as var from html or as id, not sure yet
 def sortByPrice(order):
     
-    cursor.execute('SELECT * FROM Items ORDER BY itemprice ?', order)
+    cursor.execute('SELECT * FROM Items ORDER BY itemPrice ?', order)
     sortedItems = cursor.fetchall()
 
     return render_template(''), sortedItems
@@ -140,10 +141,10 @@ def sortByPrice(order):
 
 #Order should be DESC or ASC -> Passed as a var by html? Not sure if that works
 def sortByAvailability(order):
-    if requests.method == 'POST' and 'itemId' in requests.form and 'itemQuantity' in requests.form:
+    if requests.method == 'POST' and 'itemId' in requests.form and 'quantity' in requests.form:
     #Gets item information from form
         itemId = requests['itemId']
-        itemQuantity = requests['itemQuantity']
+        quantity = requests['quantity']
 
         cursor.execute('SELECT * FROM Items ORDER BY quantity ?', order)
         sortedItems = cursor.fetchall()
