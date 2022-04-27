314632c2382bd9863bb294cc1c57474f129151e2
Natalie <69278953+infires719@users.noreply.github.com>
2022-04-26 20:49:13 -0500

    Update Shopping.py

--- Shopping.py
+++ Shopping.py
@@ -4,10 +4,12 @@ import pyodbc
 import smtplib
 import webbrowser
 import os
-from utils.UserLogin import YourClassOrFunction
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from flask import Flask, render_template, redirect, url_for, session
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from models import Items
+from models import Userlogin
+from models import Cart
+from models import Currentorders
+from django.views import View
 
 # creating nd viewing the html files in python
 app = django(__name__)
