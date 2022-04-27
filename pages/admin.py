39f736981156b39ae6d8db8babff843a08e888e1
tdizzlefizzle1 <Tylerhols@yahoo.com>
2022-04-27 18:06:34 -0500

    fixed admin stuff

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from pages.models import *
 # Register your models here.
+admin.site.register(Cart, Currentorders, Discountcodes, Items, Pastorders, Userlogins)
---
+++ pages/views/cart.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render , redirect
+
+from django.contrib.auth.hashers import  check_password
+# from pages.models import Cart
+from django.views import  View
+from pages.models import Items
+
+class Cart(View):
+    def get(self , request):
+        ids = list(request.session.get('Cart').keys())
+        products = Items.get_products_by_id(ids)
+        print(products)
+        return render(request , 'Cart.html' , {'products' : products} )
