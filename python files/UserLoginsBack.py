a1dfd1fff9248f3b6ee03dbfd5c958856ee21e93
Roberto Rodriguez-Delgado <91233281+Beboster@users.noreply.github.com>
2022-04-28 17:16:37 -0500

    Update UserLoginsBack.py
    Testing for sign up

--- python files/UserLoginsBack.py
+++ python files/UserLoginsBack.py
@@ -7,40 +7,7 @@ from pages.models import Userlogins
 from django.views import View
   
   
-class Login(View):
-    return_url = None
-  
-    def get(self, request):
-        Login.return_url = request.GET.get('return_url')
-        return render(request, 'login.html')
-  
-    def post(self, request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        username = Userlogins.get_username_by_email(email)
-        error_message = None
-        if username:
-            flag = check_password(password, username.password)
-            if flag:
-                request.session['username'] = username.id
-  
-                if Login.return_url:
-                    return HttpResponseRedirect(Login.return_url)
-                else:
-                    Login.return_url = None
-                    return redirect('homepage')
-            else:
-                error_message = 'Invalid !!'
-        else:
-            error_message = 'Invalid !!'
-  
-        print(email, password)
-        return render(request, 'login.html', {'error': error_message})
-  
-  
-def logout(request):
-    request.session.clear()
-    return redirect('login')
+
 
 class Signup (View):
     def get(self, request):
