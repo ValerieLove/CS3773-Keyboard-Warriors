48c946ab6acbc66c57ab01a0249d1055ed5c90a6
Roberto Rodriguez-Delgado <91233281+Beboster@users.noreply.github.com>
2022-04-28 17:54:09 -0500

    Update userlogins.py
    Adding

--- pages/models/userlogins.py
+++ pages/models/userlogins.py
@@ -5,3 +5,21 @@ class Userlogins(models.Model):
     email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     phonenumber = models.CharField(max_length=20)
+    
+    def register(self):
+        self.save()
+
+
+    @staticmethod
+    def get_username_by_email(email):
+        try:
+            return Userlogin.objects.get(email= email)
+        except:
+            return False
+
+
+    def isExists(self):
+        if Userlogin.objects.filter(email = self.email):
+            return True
+
+        return False
