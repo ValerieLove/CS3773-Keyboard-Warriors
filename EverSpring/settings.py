2599bfa11a01a2bffd6e2101d5e233b66c934283
tdizzlefizzle1 <Tylerhols@yahoo.com>
2022-04-29 04:03:51 +0000

    fixed settings.py

--- EverSpring/settings.py
+++ EverSpring/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["34.125.88.240", "www.everspringdesigns.tech", "everspringdesig
 # Application definition
 
 INSTALLED_APPS = [
+    "EverSpring",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
