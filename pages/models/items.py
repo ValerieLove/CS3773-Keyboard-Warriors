88a2fccd4008223d6e9a95ac1581833ace57aee9
Roberto Rodriguez-Delgado <91233281+Beboster@users.noreply.github.com>
2022-04-29 15:00:25 -0500

    Update items.py

--- pages/models/items.py
+++ pages/models/items.py
@@ -5,5 +5,4 @@ class Items(models.Model):
     itemid = models.CharField(db_column='itemID', max_length=30)  # Field name made lowercase.
     itemprice = models.DecimalField(db_column='itemPrice', max_digits=19, decimal_places=4)  # Field name made lowercase.
     quantity = models.IntegerField(db_column='Quantity')  # Field name made lowercase.
-    username = models.CharField(primary_key=True, max_length=20)
-    itemimage = models.BinaryField(db_column='itemImage')  # Field name made lowercase.
+    itemimage = models.ImageField(db_column='itemImage')  # Field name made lowercase.
