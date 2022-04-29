ef12cde4d7bf8b2c91563c9cf819f08c5097764c
Roberto Rodriguez-Delgado <91233281+Beboster@users.noreply.github.com>
2022-04-29 18:15:35 -0500

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -9,6 +9,13 @@ from .views import (
     CartPageView,
     RegisterPageView
 )
+from views.UserLoginsBack import Signup,Login,logout
+
+from .views.cart import Cart
+from .views.checkout import CheckOut
+
+from .middlewares.auth import  auth_middleware
+
 
 urlpatterns = [
     path("Men/", MenPageView.as_view(), name="Men"),
@@ -19,4 +26,11 @@ urlpatterns = [
     path("Cart/", CartPageView.as_view(), name="Cart"),
     path("Register/", RegisterPageView.as_view(), name="Register"),
     path("", HomePageView.as_view(), name="Home"),
+    path('Resister/', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout , name='logout'),
+    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
+    path('check-out', CheckOut.as_view() , name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+    
 ]
