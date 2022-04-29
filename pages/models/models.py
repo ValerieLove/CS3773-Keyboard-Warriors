48e5147dd74cc41babf0ab9f0fcb543267f693b9
Tyler Holstein <Tylerhols@yahoo.com>
2022-04-29 17:18:37 -0500

    fixed imagefield

--- pages/models/models.py
+++ pages/models/models.py
@@ -34,8 +34,7 @@ class Items(models.Model):
     itemid = models.CharField(db_column='itemID', max_length=30)  # Field name made lowercase.
     itemprice = models.DecimalField(db_column='itemPrice', max_digits=19, decimal_places=4)  # Field name made lowercase.
     quantity = models.IntegerField(db_column='Quantity')  # Field name made lowercase.
- 
-    itemimage = models.ImageField(db_column='itemImage')  # Field name made lowercase.
+    itemimage = models.ImageField(upload_to="images/items")  # Field name made lowercase.
 
 class Pastorders(models.Model):
     items = models.CharField(max_length=10)
