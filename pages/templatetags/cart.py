a7567c00476b250d8b1fd559c925e64637136bdb
Natalie <69278953+infires719@users.noreply.github.com>
2022-04-29 15:18:11 -0500

    Update cart.py

--- pages/templatetags/cart.py
+++ pages/templatetags/cart.py
@@ -4,32 +4,32 @@ register = template.Library ()
 
 
 @register.filter (name='is_in_cart')
-def is_in_cart(item, cart):
+def is_in_cart(items, cart):
     keys = cart.keys ()
     for id in keys:
-        if int (id) == item.id:
+        if int (id) == items.id:
             return True
     return False;
 
 
 @register.filter (name='cart_quantity')
-def cart_quantity(item, cart):
+def cart_quantity(items, cart):
     keys = cart.keys ()
     for id in keys:
-        if int (id) == item.id:
+        if int (id) == items.id:
             return cart.get (id)
     return 0;
 
 
 @register.filter (name='price_total')
-def price_total(item, cart):
-    return item.price * cart_quantity (item, cart)
+def price_total(items, cart):
+    return items.itemprice * cart_quantity (items, cart)
 
 
 @register.filter (name='total_cart_price')
 def total_cart_price(items, cart):
     sum = 0;
     for i in items:
-        sum += price_total (i, cart)
+        sum += itemprice_total (i, cart)
 
     return sum
