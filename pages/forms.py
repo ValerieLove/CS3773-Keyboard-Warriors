f1148e0d57b5c048224cd9f8a360b4ca20b5d7aa
tdizzlefizzle1 <Tylerhols@yahoo.com>
2022-04-30 16:30:29 -0500

    added first name last name

--- pages/forms.py
+++ pages/forms.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
-
+    phone_no = forms.CharField(max_length = 20)
+    first_name = forms.CharField(max_length = 20)
+    last_name = forms.CharField(max_length = 20)
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "first_name", "last_name", "phone_no", "password1", "password2"]
--- pages/views.py
+++ pages/views.py
@@ -42,9 +42,12 @@ class RegisterPageView(TemplateView):
 def SignUp(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
+        #if RegisterForm.password1 != RegisterForm.password2:
+        #    form = False
         if form.is_valid():
             form.save()
-        return redirect("/Home")
+            
+        return redirect("/")
     else:
         form = RegisterForm()
     return render(response, "signup.html", {"form":form})
