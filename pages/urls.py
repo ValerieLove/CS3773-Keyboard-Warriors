bdf150c1c52d3694b97ae2040aaea9950289b8b9
Tyler Holstein <Tylerhols@yahoo.com>
2022-04-29 20:52:36 -0500

    changed views

--- pages/urls.py
+++ pages/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path("Brand/", BrandPageView.as_view(), name="Brand"),
     path("Login/", LoginPageView.as_view(), name="Login"),
     path("Cart/", CartPageView.as_view(), name="Cart"),
-    path("Register/", RegisterPageView.as_view(), name="Register"),
+    #path("signup/", RegisterPageView.as_view(), name="signup"),
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("", HomePageView.as_view(), name="Home"),
 ]
