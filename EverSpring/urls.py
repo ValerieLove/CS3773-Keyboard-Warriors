07f45937afb9eafaa5fa8294d067079d0b75c3cc
Tyler Holstein <Tylerhols@yahoo.com>
2022-04-29 20:26:13 -0500

    fixed path

--- EverSpring/urls.py
+++ EverSpring/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
-
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")), #this points at our pages app and then within pages we match views to URL routes
+    path("", include("pages.urls")),
+    path("Register/", views.SignUp.as_view(), name="Register") #this points at our pages app and then within pages we match views to URL routes
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
