937fcf6a8990dcae31993633ddde9f75a099428b
Tyler Holstein <Tylerhols@yahoo.com>
2022-05-01 16:28:07 -0500

    fixed models

--- pages/models/models.py
+++ pages/models/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import User
 class Discountcodes(models.Model):
     discountcode = models.CharField(db_column='DiscountCode', primary_key=True, max_length=10)  # Field name made lowercase.
     discountpercentage = models.IntegerField(db_column='DiscountPercentage')  # Field name made lowercase.
-
+    newtotal = models.DecimalField(db_column='newTotal', max_digits=19, decimal_places=4)  # Field name made lowercase.
 class Items(models.Model):
     itemname = models.CharField(db_column='itemName', max_length=30)  # Field name made lowercase.
     itemid = models.CharField(db_column='itemID', max_length=30)  # Field name made lowercase.
