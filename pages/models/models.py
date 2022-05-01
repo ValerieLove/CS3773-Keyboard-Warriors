4f86df25b86980960b4cf89c67414d4da3cf413c
Roberto Rodriguez-Delgado <91233281+Beboster@users.noreply.github.com>
2022-05-01 14:56:38 -0500

    Update models.py

--- pages/models/models.py
+++ pages/models/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import User
 
 class Currentorders(models.Model):
     ordernumber = models.IntegerField(db_column='OrderNumber')  # Field name made lowercase.
-    items = models.CharField(max_length=20)
+    items = models.ForeignKey(Items, on_delete=models.SET_NULL, null=True)
     total = models.DecimalField(max_digits=19, decimal_places=4)
     username = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) 
     progress = models.CharField(db_column='Progress', max_length=20)  # Field name made lowercase.
